a59f1838a8714b27b196116cbda9beb92d86c5c1
Legendleo90 <akshatleo9456@gmail.com>
2023-09-04 08:35:37 +0530

    beryllium: Pixel-ify

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -1,19 +1,25 @@
 #
 # Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) 2018-2023 PixelExperience
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from beryllium device
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Pixel Specific Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := beryllium
